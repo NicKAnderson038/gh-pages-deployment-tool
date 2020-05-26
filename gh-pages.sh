8312919c1968e922b2c2913276706292d1fac96e
NicKAnderson038 <andersonnick038@gmail.com>
2020-05-26 14:34:47 -0400

    .

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,16 +1,18 @@
+STR="$(echo `jq '.scripts.build' package.json`)"
+SUB='react'
 publicPath="$(git config --get remote.origin.url | sed 's:.*/::' | cut -f1 -d".")"
 export PUBLIC_PATH="/$publicPath/"
 echo "📰 Github pages path: /$publicPath/"
 
 echo "📦 Building application"
-if [ -d "./dist" ] 
+if [[ "$STR" == *"$SUB"* ]]
 then
-    echo "VUE VUE VUE VUE VUE"
-    vue-cli-service build
-else
     echo "REACT REACT REACT REACT"
     echo "`jq '.homepage="'/$publicPath/'"' package.json`" > package.json
     react-scripts build
+else
+    echo "VUE VUE VUE VUE VUE"
+    vue-cli-service build
 fi
 echo "🏁 Build complete"
 
@@ -19,35 +21,30 @@ echo "🔙 restore path"
 
 echo "🚀 Begin deployment"
 git push origin --delete gh-pages
-if [ -d "./dist" ] 
+if [[ "$STR" == *"$SUB"* ]]
 then
-    echo "Git Add Vue application"
-    git add -f dist && git commit -m "Initial dist subtree commit" --no-verify
-    git subtree push --prefix dist origin gh-pages
-else
-    echo "Git Add React application"
     git add -f build && git commit -m "Initial build subtree commit" --no-verify
     git subtree push --prefix build origin gh-pages
+else
+    git add -f dist && git commit -m "Initial dist subtree commit" --no-verify
+    git subtree push --prefix dist origin gh-pages
 fi
 
 echo "🛁 Clean up process"
-if [ -d "./dist" ] 
+if [[ "$STR" == *"$SUB"* ]]
 then
-    echo "Remove dist folder & clean cache"
-    rm -r -v dist
-    git rm -r --cached dist
-else
-    echo "Remove build folder & clean cache"
     echo "`jq 'del(.homepage)' package.json`" > package.json
     rm -r -v build
     git rm -r --cached build
+else
+    echo "Remove dist folder & clean cache"
+    rm -r -v dist
+    git rm -r --cached dist
 fi
+
 git add .
 git commit -m "cleaned cache"
 git push
 
 exit 0
 read
-
-# HOMEPAGE="CAT"
-# echo "`jq '.homepage="'$HOMEPAGE'"' package.json`" > package.json

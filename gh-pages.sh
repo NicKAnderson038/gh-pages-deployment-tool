a41debb078249617d31847ca946914b49ed6d0af
NicKAnderson038 <andersonnick038@gmail.com>
2020-05-26 13:18:19 -0400

    .

--- gh-pages.sh
+++ gh-pages.sh
@@ -4,7 +4,7 @@ echo "📰 Github pages path: /$publicPath/"
 
 echo "📦 Building application"
 vue-cli-service build
-if [ -d "dist" ] 
+if [ -d dist ] 
 then
     vue-cli-service build
 else
@@ -12,35 +12,35 @@ else
 fi
 echo "🏁 Build complete"
 
-export PUBLIC_PATH='/'
-echo "🔙 restore path"
+# export PUBLIC_PATH='/'
+# echo "🔙 restore path"
 
-echo "🚀 Begin deployment"
-git push origin --delete gh-pages
-if [ -d "dist" ] 
-then
-    echo "Git Add Vue application"
-    git add -f dist && git commit -m "Initial dist subtree commit" --no-verify
-else
-    echo "Git Add React application"
-    git add -f build && git commit -m "Initial dist subtree commit" --no-verify
-fi
-git subtree push --prefix dist origin gh-pages
+# echo "🚀 Begin deployment"
+# git push origin --delete gh-pages
+# if [ -d dist ] 
+# then
+#     echo "Git Add Vue application"
+#     git add -f dist && git commit -m "Initial dist subtree commit" --no-verify
+# else
+#     echo "Git Add React application"
+#     git add -f build && git commit -m "Initial dist subtree commit" --no-verify
+# fi
+# git subtree push --prefix dist origin gh-pages
 
-echo "🛁 Clean up process"
-if [ -d "dist" ] 
-then
-    echo "Remove dist folder & clean cache"
-    rm -r -v dist
-    git rm -r --cached dist
-else
-    echo "Remove build folder & clean cache"
-    rm -r -v build
-    git rm -r --cached build
-fi
-git add .
-git commit -m "cleaned cache"
-git push
+# echo "🛁 Clean up process"
+# if [ -d dist ] 
+# then
+#     echo "Remove dist folder & clean cache"
+#     rm -r -v dist
+#     git rm -r --cached dist
+# else
+#     echo "Remove build folder & clean cache"
+#     rm -r -v build
+#     git rm -r --cached build
+# fi
+# git add .
+# git commit -m "cleaned cache"
+# git push
 
 exit 0
 read

673bcbcf2f5ebc531deeece45362d18c4c9c5621
NicKAnderson038 <andersonnick038@gmail.com>
2020-05-26 13:02:20 -0400

    🎉 first commit

---
+++ gh-pages.sh
@@ -0,0 +1,40 @@
+publicPath="$(git config --get remote.origin.url | sed 's:.*/::' | cut -f1 -d".")"
+export PUBLIC_PATH="/$publicPath/"
+echo "📰 Github pages path: /$publicPath/"
+
+echo "📦 Building application"
+vue-cli-service build
+echo "🏁 Build complete"
+
+export PUBLIC_PATH='/'
+echo "🔙 restore path"
+
+echo "🚀 Begin deployment"
+git push origin --delete gh-pages
+if [ -d "./dist" ] 
+then
+    echo "Git Add Vue application"
+    git add -f dist && git commit -m "Initial dist subtree commit" --no-verify
+else
+    echo "Git Add React application"
+    git add -f build && git commit -m "Initial dist subtree commit" --no-verify
+fi
+git subtree push --prefix dist origin gh-pages
+
+echo "🛁 Clean up process"
+if [ -d "./dist" ] 
+then
+    echo "Remove dist folder & clean cache"
+    rm -r -v dist
+    git rm -r --cached dist
+else
+    echo "Remove build folder & clean cache"
+    rm -r -v build
+    git rm -r --cached build
+fi
+git add .
+git commit -m "cleaned cache"
+git push
+
+exit 0
+read
